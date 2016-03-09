1f2f88ed9ad3a4837a7828cee891d1c5f175856f
joetww <joetww@users.noreply.github.com>
2016-03-09 15:50:27 +0800

    有的監控端透過特別的方法綁定IP，所以不一定是對應到localhost
    有的監控端透過特別的方法綁定IP，所以不一定是對應到localhost

--- MagConsole.sh
+++ MagConsole.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/bash
 #反向ssh tunnel + socat 轉向snmp port供 cacti用
+#
+#有的監控端透過特別的方法綁定IP，所以不一定是對應到localhost
+#
 
 REMOTE_CONNECT="maxi@52.193.0.52"
 REMOTE_PORT=9100
 REMOTE_SOCAT_DIR="~/socat_log/"
 MONITOR_PORT=20000
 LOCAL_PORT=9000
+LOCAL_IP="localhost" ## 有時候不一定綁定到localhost(127.0.0.1)
+
 
 AUTOSSH_PIDFILE="/tmp/MagConsole.AutoSSH.pid"
 export AUTOSSH_PIDFILE
@@ -14,8 +19,8 @@ export AUTOSSH_LOGFILE
 
 
 test \! -f  ${AUTOSSH_PIDFILE} && \
-/usr/bin/autossh -M ${MONITOR_PORT} -NfR ${REMOTE_PORT}:localhost:${LOCAL_PORT} ${REMOTE_CONNECT}
+/usr/bin/autossh -M ${MONITOR_PORT} -NfR ${REMOTE_PORT}:${LOCAL_IP}:${LOCAL_PORT} ${REMOTE_CONNECT}
 
-nohup socat -s tcp4-listen:${LOCAL_PORT},reuseaddr,fork UDP:127.0.0.1:161 > /tmp/MagConsole.socat.out.log 2> /tmp/MagConsole.socat.error.log < /dev/null &
+nohup socat -s tcp4-listen:${LOCAL_PORT},reuseaddr,fork UDP:${LOCAL_IP}:161 > /tmp/MagConsole.socat.out.log 2> /tmp/MagConsole.socat.error.log < /dev/null &
 
 ssh ${REMOTE_CONNECT} "test \! -d ${REMOTE_SOCAT_DIR} && mkdir -p ${REMOTE_SOCAT_DIR}; cd ~/socat_log && sh -c '(( nohup socat udp4-RECVFROM:'$(expr ${REMOTE_PORT} + 61)',reuseaddr,fork tcp:localhost:'${REMOTE_PORT}' > '${REMOTE_SOCAT_DIR}'/'$(hostname)'.socat.out.log 2> '${REMOTE_SOCAT_DIR}'/'$(hostname)'.socat.error.log < /dev/null ) & )'"
