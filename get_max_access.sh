97340c3e58c15c86c99bea55b6376623a605d05c
游小爸 <joetww@users.noreply.github.com>
2017-11-14 14:26:35 +0800

    修改access log, 省略一些各個前端站抓資料的步驟了

--- get_max_access.sh
+++ get_max_access.sh
@@ -20,10 +20,10 @@ BADSTRINGCOMMAND=`join_by \| ${BADSTRING}`
 IFS=${OLDIFS};
 
 WHITELIST="
-    1.2.3.4
-    5.6.7.8
-    8.8.8.8
-    8.8.4.4
+    133.130.124.49
+    106.104.136.243
+    1.171.159.58
+    1.171.160.60
 "
 VERYBADLIST="
     182.118.33.6
@@ -44,32 +44,44 @@ echo "$BADSTRINGCOMMAND"
 
 echo
 NEW_BAD_LIST=$(
-    c=1;
-    (
-    for i in `cat ~/seoproxylist.txt | grep -v ^$ |  head -n 20 `
-    do
-        >&2 printf "proxy %02d\t%-15s\n" ${c} ${i};
-        (
-            ssh -o StrictHostKeyChecking=no joeyue@${i} "function join_by { local IFS=\"\$1\"; shift; echo \"\$*\"; }; TIMELOG=\$(join_by \| \$(for i in {1..10}; do date -d \"\${i} minute ago \" +\"%d/%b/%Y:%H:%M\"; done)); sudo cat /usr/local/webserver/openresty/nginx/logs/access | ( grep -P '('\${TIMELOG}')' ) | awk '{print \$1}' | gzip ;" ;
-        );
-        c=`expr ${c} + 1`;
-    done
-    ) | zcat | sort | uniq -c | sort -n | awk '$1 > 5000{print $2}';
+    # 原本是到各前端proxy去蒐集連線IP, 現在改從後端直接收集了
+    # CONNECT_COUNT=$((
+        # c=1;
+        # for i in `cat ~/seoproxylist.txt | grep -v ^$ |  head -n 20 `
+        # do
+            # >&2 printf "proxy %02d\t%-15s\n" ${c} ${i};
+            # (
+                # ssh -o StrictHostKeyChecking=no joeyue@${i} "function join_by { local IFS=\"\$1\"; shift; echo \"\$*\"; }; TIMELOG=\$(join_by \| \$(for i in {1..10}; do date -d \"\${i} minute ago \" +\"%d/%b/%Y:%H:%M\"; done)); sudo cat /usr/local/webserver/openresty/nginx/logs/access | ( grep -P '('\${TIMELOG}')' ) | awk '{print \$1}' | gzip ;" ;
+            # );
+            # c=`expr ${c} + 1`;
+        # done
+    # ) | zcat | sort | uniq -c | sort -n );
+   
+    TIMELOG=$(join_by \| $(for i in {1..10}; do date -d "${i} minute ago " +"%d/%b/%Y:%H:%M"; done));
+    #>&2 echo ${TIMELOG}
+    CONNECT_COUNT=$(sudo cat /var/log/httpd/forward_log | grep -P '('${TIMELOG}')' | awk '{print $2}' | sort | uniq -c | sort -n) ; 
+    echo "${CONNECT_COUNT}" | awk '$1 > 1000{print $2}' ;
+    >&2 echo "連線次數:";
+    echo "${CONNECT_COUNT}" | >&2  tail -n 5 ;
+
+    FAKEDOMAIN=$(sudo cat /var/log/httpd/forward_log | grep -P '('${TIMELOG}')' | grep -v DNSPod-Monitor | grep -P '^([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print $2}' | sort -u)
+    echo "${FAKEDOMAIN}" ;
+    >&2 echo "連入FAKEDOMAIN" ;
+    echo "${FAKEDOMAIN}" | >&2  tail -n 5 ;
 
     ( 
     for i in `cat ~/seoproxylist.txt | grep -v ^$ |  head -n 20 `
     do
         (
             ssh -o StrictHostKeyChecking=no joeyue@${i} "sudo cat /usr/local/webserver/openresty/nginx/logs/access | grep -P '($(date +"%d/%b/%Y")|$(date -d "1 day ago" +"%d/%b/%Y"))' | grep -P '(${BADSTRINGCOMMAND})' | awk '{print \$1}' | sort -u | gzip ; ";
-            ssh -o StrictHostKeyChecking=no joeyue@${i} "function join_by { local IFS=\"\$1\"; shift; echo \"\$*\"; }; TIMELOG=\$(join_by \| \$(for i in {1..10}; do date -d \"\${i} minute ago \" +\"%d/%b/%Y:%H:%M\"; done)); sudo cat /usr/local/webserver/openresty/nginx/logs/access | grep -P '.*?([0-9]{1,3}\.){3}[0-9]+$' | grep -P '('\${TIMELOG}')' | awk '{print \$1}' | sort -nu | gzip ;" ;
         );
     done
     ) | zcat | sort -un ;
 ) ;
 #echo "${NEW_BAD_LIST}"
-BLACKLIST_SETTING=$(
-    ssh -o StrictHostKeyChecking=no joeyue@`cat ~/seoproxylist.txt | grep -v ^$ | head -n 1 ` "sudo /usr/sbin/ipset save blacklist"
-)
+#BLACKLIST_SETTING=$(
+#    ssh -o StrictHostKeyChecking=no joeyue@`cat ~/seoproxylist.txt | grep -v ^$ | head -n 1 ` "sudo /usr/sbin/ipset save blacklist"
+#)
 
 echo ;
 for i in {1..30}; do printf "="; done
@@ -80,26 +92,25 @@ RENEW_BLACKLIST=$(
     do
         printf "%s %-15s %s %-6d ;\n" "sudo /usr/sbin/ipset -exist add blacklist " ${i} "timeout" `expr 24 \* 60 \* 60`
     done | grep -v -P '('$GREPCOMMAND')' ;
-    for i in $WHITELIST;
-    do
-        printf "sudo ipset save | grep --silent \"^add blacklist %s\" && %s %-15s ;\n" ${i} "sudo /usr/sbin/ipset del blacklist " ${i} ;
-    done
     for i in $(
         echo "${NEW_BAD_LIST}" | grep -v -P '('$GREPCOMMAND')' | grep -v -P '('$(join_by \| ${VERYBADLIST})')' ;
     );
     do
         printf "sudo ipset save | grep --silent \"^add blacklist %s\" || %s %-15s ;\n" ${i} "sudo /usr/sbin/ipset add blacklist " ${i} ;
-    done | sort -u
+    done | sort -u ;
+    for i in $WHITELIST;
+    do
+        printf "sudo ipset save | grep --silent \"^add blacklist %s\" && %s %-15s ;\n" ${i} "sudo /usr/sbin/ipset del blacklist " ${i} ;
+    done ;
 )
 
-echo "("$(echo "${RENEW_BLACKLIST}" | head -n 30 | wc -l)"/"$(echo "${RENEW_BLACKLIST}" | wc -l)")"
-echo "${RENEW_BLACKLIST}" | head -n 30;
-
+echo "("$(echo "${RENEW_BLACKLIST}" | head -n 100 | wc -l)"/"$(echo "${RENEW_BLACKLIST}" | wc -l)")"
+echo "${RENEW_BLACKLIST}" | head -n 100 ;
+echo "${RENEW_BLACKLIST}" | grep -A 3 -B 3 220.135.106.53
 
 echo 
 echo "塞資料入proxy";
 for i in `cat ~/seoproxylist.txt | grep -v ^$ |  head -n 20 `
 do
-    echo .
     ssh -o StrictHostKeyChecking=no joeyue@${i} "${RENEW_BLACKLIST}"
 done
