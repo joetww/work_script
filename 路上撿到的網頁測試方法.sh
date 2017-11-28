95875c89a787b5f018f9f837672dc70629513aeb
游小爸 <joetww@users.noreply.github.com>
2017-11-28 10:00:51 +0800

    Create 路上撿到的網頁測試方法.sh

---
+++ 路上撿到的網頁測試方法.sh
@@ -0,0 +1,1 @@
+curl -s -w 'Testing Website Response Time for :%{url_effective}\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null http://www.google.com/
